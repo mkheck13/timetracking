443371ade323ace4ecfd3fea2fa8b5eec9460106
mkheck13 <mkheck13@gmail.com>
2024-12-04 08:08:11 -0800

    card work

--- scripts/app.js
+++ scripts/app.js
@@ -1,41 +1,21 @@
-// Linking JSON Data
-const getData = '../data/data.json';
+
 
 // Setting variables
 let dailyBtn = document.getElementById("dailyBtn");
 let weeklyBtn = document.getElementById("weeklyBtn");
 let monthlyBtn = document.getElementById("monthlyBtn");
-let cardContainer = document.querySelector(".card-container");
 
 // Fetching the data
-fetch(getData)
-.then(response => response.json())
-.then(data => displayData(data))
-console.log(getData);
-
-let displayData = (data) =>{
-    window.addEventListener('load', loadDailyData(data));
-
-    dailyBtn.addEventListener('click', () => {
-        cardContainer.innerHTML = '';
-        loadDailyData(data);
-    });
-
-    // weeklyBtn.addEventListener('click', () => {
-    //     cardContainer.innerHTML = '';
-    //     loadWeeklyData(data);
-        
-    // });
-
-    // monthlyBtn.addEventListener('click', () => {
-    //     cardContainer.innerHTML = '';
-    //     loadMonthlyData(data);
-    // });
+function getData() {
+    fetch('../data/data.json')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            return data;
+        })
 };
+getData();
 
-let loadDailyData = (data) =>{
-    data.map(element =>{
-        cardContainer.innerHTML += `
-        `
-    })
-}
+
+
+
